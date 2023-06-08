66971d9ed35314ccdc5abae3d2fcc5337d5a2b67
jspl151 <jayasutha151@gmail.com>
2023-06-08 08:54:29 +0000

    Adding marks in a array using reduce.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const animals = ["dog", "cat", "wolf", "lion", "dog", "cat", "wolf", "lion", "dog", "tiger"];
+const {map,values,reduce} = require('@laufire/utils/collection');
 
-let countObject = animals.reduce((acc,currentValue)=>
- {
-    return (
-        acc[currentValue] ? acc[currentValue]+1 : (acc[currentValue] = 1),
-        acc
-    );
-},
-{});
-console.log(countObject);
+const marks = [78,98,46,67,38];
+
+const population = 
+{
+  male : 50,
+  female :45,
+  others :5
+}
+const data = values(map(population,human => human));
+console.log(data);
+const data1 = map(population,human => 
+({
+  value : human*2 ,
+}));
+console.log(data1);
+const totalPopulation =reduce(population,(acc,cur)=>acc+cur,0);
+const total = reduce(marks,(acc,cur)=> 
+ ({
+    total : acc.total+cur }),{total:0});
+
+console.log(totalPopulation);
+console.log(total);
